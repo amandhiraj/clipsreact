9ef84cbc19cb2af956bf0b3fc79d21b63cade54f
amandhiraj <amandhiraj@live.ca>
2025-07-11 14:19:08 -0400

    added new func to add clips

--- backend/app/routes/clips.py
+++ backend/app/routes/clips.py
@@ -5,11 +5,15 @@ from sqlalchemy import func
 from app.models import Clip
 from app.db import get_session
 from typing import List
+import json
 
 router = APIRouter(prefix="/clips", tags=["Clips"])
 
 @router.post("/")
 def create_clip(clip: Clip, session = Depends(get_session)):
+    # convert list to JSON string before saving
+    if isinstance(clip.tags, list):
+        clip.tags = json.dumps(clip.tags)
     session.add(clip)
     session.commit()
     session.refresh(clip)
